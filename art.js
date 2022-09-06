57aed69f062f58666edf665cc578d3e955337982
anjay <63073955+Anjay209@users.noreply.github.com>
2022-09-06 18:54:42 +0000

    I updated the classes and added topics taught

--- art.js
+++ art.js
@@ -1,4 +1,4 @@
-/*scroll top top*/
+/*scroll tI op top*/
 const scrollButton = document.querySelector(".scroll-top");
 scrollButton.addEventListener("click", () => {
   window.scrollTo(0, 0);
@@ -23,3 +23,55 @@ const position = document.documentElement;
 position.addEventListener("mousemove", e => {
   position.style.setProperty('--x', e.clientX + 'px');
 })
+
+/*
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+*/
+
+function view_topics(bId,mId,cId){
+  // Get the modal
+var modal = document.getElementById(mId);
+
+// Get the button that opens the modal
+var btn = document.getElementById(bId);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName(cId)[0];
+  
+modal.style.display = "block";
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
